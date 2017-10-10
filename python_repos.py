4628ad984480c2b8cd3a1bb8e93aa33ebf313923
Alex Vasko <vaskoaj@gmail.com>
2017-10-10 15:44:28 -0400

    initial commit

---
+++ python_repos.py
@@ -0,0 +1,25 @@
+import requests
+
+#make an API call and store the response
+url= 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+r = requests.get(url)
+print("Status Code:" ,r.status_code)
+
+#store api response in a variable
+response_dict = r.json()
+print("Total repositories:", response_dict['total_count'])
+
+#explore information about repositories
+repo_dicts = response_dict['items']
+print("Repositories return:", len(repo_dicts))
+
+#examine the first repository
+repo_dict = repo_dicts[0]
+
+print("\nSelected information about each repository:")
+for repo_dict in repo_dicts:
+	print("Name:", repo_dict['name'])
+	print('Owner:', repo_dict['owner']['login'])
+	print('Stars:', repo_dict['stargazers_count'])
+	print('Repository:', repo_dict['html_url'])
+	print('Description:', repo_dict['description'])

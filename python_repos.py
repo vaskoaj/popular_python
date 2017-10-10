9cd10ad1e1f8852231cf74c42ab869b8e2eb2d66
Alex Vasko <vaskoaj@gmail.com>
2017-10-10 16:19:11 -0400

    added links and descriptions

--- python_repos.py
+++ python_repos.py
@@ -1,29 +1,36 @@
 import requests
 import pygal
-from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS 
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 
-#make an API call and store the response
-url= 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+#make an API call, and store the response.
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
 r = requests.get(url)
-print("Status Code:" ,r.status_code)
+print("Status code:", r.status_code)
 
-#store api response in a variable
+#store API response in a variable.
 response_dict = r.json()
 print("Total repositories:", response_dict['total_count'])
 
-#explore information about repositories
+#explore information about the repositories.
 repo_dicts = response_dict['items']
-print("Repositories return:", len(repo_dicts))
 
-#examine the first repository
-repo_dict = repo_dicts[0]
-
-names, stars = [], []
+names, plot_dicts = [], []
 for repo_dict in repo_dicts:
-	names.append(repo_dict['name'])
-	stars.append(repo_dict['stargazers_count'])
+    names.append(repo_dict['name'])
 
-#make visualization
+    #get the project description, if one is available.
+    description = repo_dict['description']
+    if not description:
+        description = "No description provided."
+
+    plot_dict = {
+        'value': repo_dict['stargazers_count'],
+        'label': description,
+        'xlink': repo_dict['html_url'],
+        }
+    plot_dicts.append(plot_dict)
+
+#make visualization.
 my_style = LS('#333366', base_style=LCS)
 my_style.title_font_size = 24
 my_style.label_font_size = 14
@@ -40,5 +47,5 @@ chart = pygal.Bar(my_config, style=my_style)
 chart.title = 'Most-Starred Python Projects on GitHub'
 chart.x_labels = names
 
-chart.add('', stars)
+chart.add('', plot_dicts)
 chart.render_to_file('python_repos.svg')
